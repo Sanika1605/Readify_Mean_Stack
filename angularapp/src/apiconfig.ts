3f57e15a0c7333635fdc0d0cf86cf2e760fe46ee
sanikagpariyal1616 <sanikagpariyal1616@gmail.com>
2026-07-28 12:28:25 +0530

    Fix Render deployment

--- angularapp/src/apiconfig.ts
+++ angularapp/src/apiconfig.ts
@@ -1,1 +1,4 @@
-export const apiConfig={apiUrl:'http://localhost:3000'}
+// export const apiConfig={apiUrl:'http://localhost:3000'}
+export const apiConfig = {
+  apiUrl: 'https://readify-mean-stack.onrender.com'
+};
